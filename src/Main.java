7c18a0871d4ee09fa3926b79734c9b76db37fed3
Dima <dimapanteleyvrach@gmail.com>
2023-12-19 03:17:17 +0300

    готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import exteption.DivisionByZeroExption;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            division( 5,0);
+        }catch (DivisionByZeroExption e){
+            System.out.println("На ноль делить нельзя");
+        }
     }
-} //
+    public static void division( int a, int b){
+        if (b==0)
+            throw new DivisionByZeroExption();
+        System.out.println(a/b);
+    }
+}
